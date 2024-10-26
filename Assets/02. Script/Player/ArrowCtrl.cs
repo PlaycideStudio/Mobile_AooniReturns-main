b2646eaa9bcb2084847216b2ca9cd3287129d1b9
PlaycideStudio <90382701+PlaycideStudio@users.noreply.github.com>
2024-10-26 15:35:59 +0900

    24-10-25 / Update 3
    - Projectile 업데이트(속도 / 거리 구분)

--- Assets/02. Script/Player/ArrowCtrl.cs
+++ Assets/02. Script/Player/ArrowCtrl.cs
@@ -8,14 +8,16 @@ public class ArrowCtrl : NetworkBehaviour
 {
     [SerializeField] Animator animCtrl;
 
+    private Projectiles selected_projectile;
     private Vector3 startPosition = new(0, 0.7f, 0);
-
     private CharacterCtrl myCharCtrl;
 
     private void Start()
     {
         gameObject.SetActive(false);
         myCharCtrl = GetComponentInParent<CharacterCtrl>();
+
+        RPC_SelectedProjectile();
     }
 
     public void FireArrow()
@@ -29,17 +31,36 @@ public class ArrowCtrl : NetworkBehaviour
         gameObject.SetActive(true);
 
         Vector2 normalizedDir = NormalizeDirection(_dir);
-
-        Vector3 targetPosition = transform.position + new Vector3(normalizedDir.x, normalizedDir.y, 0) * 2;
+        Vector3 targetPosition = transform.position + new Vector3(normalizedDir.x, normalizedDir.y, 0) * Projectile.projectile.distance;
 
         animCtrl.SetFloat("MoveX", normalizedDir.x);
         animCtrl.SetFloat("MoveY", normalizedDir.y);
 
-        transform.DOMove(targetPosition, 0.5f).SetEase(Ease.Linear).OnComplete(() =>
+        float distance = Vector3.Distance(transform.position, targetPosition);
+        float duration = distance / Projectile.projectile.speed;
+
+        transform.DOMove(targetPosition, duration).SetEase(Ease.Linear).OnComplete(() =>
         {
             gameObject.SetActive(false);
             transform.localPosition = startPosition;
         });
+    }
+
+    [Rpc]
+    void RPC_SelectedProjectile()
+    {
+        NetworkObject playerObject = Runner.GetPlayerObject(Runner.LocalPlayer);
+        string playerName = playerObject != null ? playerObject.name : "Unknown Player";
+
+
+        switch (selected_projectile)
+        {
+            case 0:
+                Projectile.projectile.shooter_name = playerName;
+                Projectile.projectile.distance = 2;
+                Projectile.projectile.speed = 1.1f;
+                break;
+        }
     }
 
     private Vector2 NormalizeDirection(Vector2 dir)
---
+++ Assets/02. Script/Player/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Projectiles
+{
+    arrow, //기본 스킨
+}
+
+public class Projectile : MonoBehaviour
+{
+    public static Projectile projectile = null;
+
+    public string shooter_name;
+    public float distance;
+    public float speed;
+    public Vector2 direction;
+    public List<string> hitted_unit;
+    public Sprite icon;
+
+    private void Awake()
+    {
+        if (null == projectile)
+        {
+            projectile = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
